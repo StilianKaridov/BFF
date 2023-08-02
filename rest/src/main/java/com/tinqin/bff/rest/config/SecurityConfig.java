18ca34dde43ba48091903928c41607d0d34592a1
StilianKaridov <karidovstilian@gmail.com>
2023-08-02 12:58:46 +0300

    Deleted the logoutHandler

--- rest/src/main/java/com/tinqin/bff/rest/config/SecurityConfig.java
+++ rest/src/main/java/com/tinqin/bff/rest/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +20,6 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {

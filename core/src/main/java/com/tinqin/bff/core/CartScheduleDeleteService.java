50503561576a29fb090bb60d0fa48996ba778a0c
StilianKaridov <karidovstilian@gmail.com>
2023-08-08 14:03:03 +0300

    Made separate helper service for scheduled removing items from cart

---
+++ core/src/main/java/com/tinqin/bff/core/CartScheduleDeleteService.java
@@ -0,0 +1,29 @@
+package com.tinqin.bff.core;
+
+import com.tinqin.bff.persistence.repository.ShoppingCartRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Service
+public class CartScheduleDeleteService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    public CartScheduleDeleteService(ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 * * * *")
+    public void deleteCartIfNotSoldWithinOneWeek() {
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
+        Timestamp timestamp = Timestamp.valueOf(oneWeekAgo);
+        this.shoppingCartRepository.deleteAllByAddedOnBefore(timestamp);
+    }
+}

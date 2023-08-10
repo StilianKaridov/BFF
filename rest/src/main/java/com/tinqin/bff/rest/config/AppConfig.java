b25bce25144fb7a8f9b7c95c62f6b877bbf2b154
StilianKaridov <karidovstilian@gmail.com>
2023-08-10 10:14:41 +0300

    Removed unused imports

--- rest/src/main/java/com/tinqin/bff/rest/config/AppConfig.java
+++ rest/src/main/java/com/tinqin/bff/rest/config/AppConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 @RequiredArgsConstructor
